e3dbd208f416e696d5aa08dc852c8e6c98b11c3d
Deepak Yadav <deepaky.tt.19@nitj.ac.in>
2023-12-20 20:47:33 +0530

    mongodb connection

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,19 @@
 import { App } from "@src/app";
+import MongooseConnection from "./config/dbConnection";
+import { db } from "./config/configManager";
+import { AppLogger } from "./core/Logger";
 
 const app = new App();
+
+const mongooseConnection = new MongooseConnection({
+    mongoUrl: db.url,
+    onStartConnection: mongoUrl => AppLogger.info('Connecting to MongoDB at', mongoUrl),
+    onConnectionError: error => AppLogger.error('Error made which trying to make connection', error),
+    onConnectionRetry: mongoUrl => AppLogger.info('Retrying to MongoDB at', mongoUrl)
+});
+
+mongooseConnection.connect(mongoUrl => {
+    AppLogger.info("Connection made with: ", mongoUrl);
+});
+
 app.run();
