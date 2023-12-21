ff525923de878be99cc5260edd570e04a5b58b92
Deepak Yadav <deepaky.tt.19@nitj.ac.in>
2023-12-21 13:56:41 +0530

    Added user repo

--- src/controllers/LoginController/index.ts
+++ src/controllers/LoginController/index.ts
@@ -1,18 +1,20 @@
 import { Router, Request, Response } from 'express';
 import { AppRoute } from '@src/appRouting';
-import { Api } from '@src/core/API_Handler/ResponseHelper';
-import validator from '@src/validation/validator';
-import login from '@src/validation/schema/login';
+// import { Api } from '@src/core/API_Handler/ResponseHelper';
+// import validator from '@src/validation/validator';
+// import login from '@src/validation/schema/login';
+import { UserRepo } from '@src/dao/repository/UserRepo';
 
 export class LoginController implements AppRoute {
   public route = '/login';
   public router: Router = Router();
 
   constructor() {
-    this.router.get('/', validator(login.credential), this.getLoggedIn);
+    this.router.post('/', this.getLoggedIn);
   }
 
   private async getLoggedIn(request: Request, response: Response):Promise<any> {
-    return Api.ok(request, response, "I am back");
+    const user = new UserRepo().createUser(request.body);
+    response.status(201).json(user);
   }
 }
--- src/dao/model/user.ts
+++ src/dao/model/user.ts
@@ -1,4 +1,7 @@
-import { Schema, model, Date, Model } from 'mongoose';
+import mongoose, { Schema, model, Date, Model } from 'mongoose';
+import bcrypt from 'bcrypt';
+
+import { saltHashRounds } from '@src/config/configManager';
 
 export const enum IRole {
   Admin = 'admin',
@@ -7,37 +10,45 @@ export const enum IRole {
   Parent = 'parent',
 }
 
-export interface IUserModel {
+export interface IUser{
   firstName: string;
   lastName: string;
   userId: number;
   role: IRole;
   profileUrl: string;
   email: string;
+  isEmailVerified:boolean;
   password: string;
   createdAt: Date;
   updatedAt: Date;
   accessToken: string[];
   refreshToken: string[];
 }
-
-interface IUserIdSeqModel extends Model<IUserModel> {
-  getNextUserId(userId: number, data: any): any;
+interface IUserDoc extends IUser, Document{
+  isPasswordMatch(password:string): Promise<boolean>;
 }
 
-const schema = new Schema<IUserModel>(
+interface IUserModel extends Model<IUserDoc> {
+  getNextUserId(userId: number, data: any): any;
+  isEmailTaken(email: string, excludeUserId?: mongoose.Types.ObjectId): Promise<boolean>;
+}
+
+const validateEmail = (email: string) => /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g.test(email);
+
+const schema = new Schema<IUserDoc, IUserModel>(
   {
     firstName: {
       type: String,
       required: true,
+      trim: true
     },
     lastName: {
       type: String,
       required: true,
+      trim: true
     },
     userId: {
       type: Number,
-      required: true,
       index: true,
       unique: true,
     },
@@ -45,16 +56,26 @@ const schema = new Schema<IUserModel>(
       type: String,
       required: true,
       index: true,
+      enum: [IRole.Admin, IRole.Teacher, IRole.Student, IRole.Parent]
     },
     profileUrl: {
       type: String,
-      required: true,
+      trim: true
     },
     email: {
       type: String,
       required: true,
       index: true,
       unique: true,
+      trim: true,
+      validate:{
+        validator: (value:string) => validateEmail(value),
+        message: (props) => `${props.value} is not a valid email!`
+      }
+    },
+    isEmailVerified:{
+      type: Boolean,
+      default: false
     },
     password: {
       type: String,
@@ -64,14 +85,30 @@ const schema = new Schema<IUserModel>(
   { timestamps: true },
 );
 
-const User = model<IUserModel, IUserIdSeqModel>('User', schema);
-
 schema.statics.getNextUserId = async function (userId, data) {
   return this.findOneAndUpdate(
     { userId },
-    { $set: { ...data } },
+    { $set: { ...data }, $setOnInsert: { userId }},
     { upsert: true, new: true },
   ).exec();
 };
+
+schema.statics.isEmailTaken = async function (email: string, excludeUserId: mongoose.ObjectId) {
+  return !!await this.findOne({ email, _id: { $ne: excludeUserId } });
+}
+
+schema.methods.isPasswordMatch = async function(password:string){
+  return bcrypt.compare(password, this.password);
+};
+
+schema.pre('save', async function(next){
+  if(this.isModified('password')){
+    const saltRounds:number = parseInt(saltHashRounds || '8');
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+  next();
+});
+
+const User = model<IUserDoc, IUserModel>('User', schema);
 
 export default User;
