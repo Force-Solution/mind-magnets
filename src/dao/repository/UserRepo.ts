b78b112d48ec424b48ed4a248c9339c19ad592c9
Deepak Yadav <deepaky.tt.19@nitj.ac.in>
2023-12-21 18:44:14 +0530

    code fix

--- src/dao/repository/UserRepo.ts
+++ src/dao/repository/UserRepo.ts
@@ -1,9 +1,10 @@
+import { BadRequestError, message } from '@src/core/API_Handler/ApiError';
 import User, { IUser } from '@src/dao/model/user';
 
 export class UserRepo {
   public async createUser(userBody: IUser) {
     if (await this.getUserByEmail(userBody.email)) {
-        // throw  Api.invalid()
+        throw new BadRequestError(message.DuplicateEmail);
     }
     return User.create(userBody);
   };
