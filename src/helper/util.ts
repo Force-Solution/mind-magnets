a4a1a07a2ff7c85507e114c47d6a189e5a6381b5
MULAYAM SINGH YADAV <msy8795@gmail.com>
2023-12-28 19:52:01 +0530

    Adding userdetails api

--- src/helper/util.ts
+++ src/helper/util.ts
@@ -5,8 +5,6 @@ export const constructDateFromUnix = (unix:string | number): Date =>{
     return new Date(parsed);
 }
 
-export const passwordRegex: RegExp =
-  /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+export const passwordRegex: RegExp = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
 
-export const validateEmail = (email: string) =>
-  /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g.test(email);
+export const validateEmail = (email: string) =>/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g.test(email);
