401b7cdd30c2aa1eec220b34b7971a05a652fb99
MULAYAM SINGH YADAV <msy8795@gmail.com>
2024-01-02 21:58:19 +0530

    fixing swagger

--- src/docs/swaggerConfig.ts
+++ src/docs/swaggerConfig.ts
@@ -1,19 +1,10 @@
-import { SwaggerOptions } from 'swagger-ui-express';
 import YAML from 'yamljs';
 import path from 'path';
-// const options: swaggerJsdoc.Options = {
-// //   swaggerDefinition: {
-// //     openapi: '3.0.0',
-// //     info: {
-// //       title: 'APNI-Coaching',
-// //       version: '1.0.0',
-// //       description: 'This is backend following MVC architeture of Apni-Coaching server',
-// //     },
-// //   },
-// //   apis: ['./routes/*.ts'], 
-// // };
+import swaggerJsdoc from 'swagger-jsdoc';
+const swaggerDefinitions = YAML.load(path.join(__dirname, 'swagger.yaml'));
+const options = {
+  swaggerDefinition: swaggerDefinitions,
 
-// export const specs = swaggerJsdoc(options);
-console.log(path.join(__dirname, "swagger.yaml"));
-
-export const specs: SwaggerOptions = YAML.load(path.join(__dirname, "swagger.yaml"));
+  apis: ['./src/controllers/*.ts'],
+};
+export const specs = swaggerJsdoc(options);
