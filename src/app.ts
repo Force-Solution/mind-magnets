b9c3de953bcea4e5156bc341783fc6b7f08e7127
Deepak Yadav <deepaky.tt.19@nitj.ac.in>
2024-02-18 12:50:18 +0530

    added options

--- src/app.ts
+++ src/app.ts
@@ -40,17 +40,12 @@ export class App {
 
   private configureMiddleware() {
     AppLogger.configureLogger();
+    this.app.use(cors());
+    this.app.options('*', this.configureOptions);
     this.app.use(json({ limit: '50mb' }));
     this.app.use(urlencoded({ limit: '50mb', extended: true }));
     this.app.use(logger); // log request
     this.app.use(morganMiddleware);
-    this.app.use(
-      cors({
-        origin: '*',
-        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-        credentials: true,
-      }),
-    );
     this.app.use(rateLimiter(rateLimiting)); // for now I have put common, segregate on single route when required
   }
 
@@ -88,6 +83,13 @@ export class App {
       return Api.notFound(request, response, error);
     });
   }
+
+  private configureOptions(_request: Request, response: Response){
+    response.header('Access-Control-Allow-Origin', '*');
+    response.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    response.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    response.send(200);
+  } 
 
   public run() {
     this.app.listen(port);
