eb32dda0710892829c8f4b0808a6cfc045705620
Deepak Yadav <deepaky.tt.19@nitj.ac.in>
2024-02-18 15:03:17 +0530

    code fix

--- src/app.ts
+++ src/app.ts
@@ -47,6 +47,21 @@ export class App {
     this.app.use(logger); // log request
     this.app.use(morganMiddleware);
     this.app.use(rateLimiter(rateLimiting)); // for now I have put common, segregate on single route when required
+    this.app.use(function(req, res, next) {
+      res.header('Access-Control-Allow-Origin', '*');
+      res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
+      res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+  
+      //intercepts OPTIONS method
+      if ('OPTIONS' === req.method) {
+        //respond with 200
+        res.send(200);
+      }
+      else {
+      //move on
+        next();
+      }
+  });
   }
 
   private configureBaseRoute() {
