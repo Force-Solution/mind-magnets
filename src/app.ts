1d6c58cfab04ebec0c7ae74b7c1d3530052509c3
Deepak Yadav <deepaky.tt.19@nitj.ac.in>
2024-02-19 21:27:18 +0530

    pushed

--- src/app.ts
+++ src/app.ts
@@ -85,7 +85,7 @@ export class App {
   }
 
   private configureOptions(_request: Request, response: Response) {
-    response.header('Access-Control-Allow-Origin', '*');
+    response.header('Access-Control-Allow-Origin', 'http://localhost:3000');
     response.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     response.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
     response.sendStatus(200);
