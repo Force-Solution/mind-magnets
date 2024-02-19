e77106b233ed84e6abf37767fb6c0772345c5a43
Deepak Yadav <deepaky.tt.19@nitj.ac.in>
2024-02-19 21:15:57 +0530

    code fix

--- src/app.ts
+++ src/app.ts
@@ -84,12 +84,12 @@ export class App {
     });
   }
 
-  private configureOptions(_request: Request, response: Response){
+  private configureOptions(_request: Request, response: Response) {
     response.header('Access-Control-Allow-Origin', '*');
     response.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     response.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-    response.send(200);
-  } 
+    response.sendStatus(200);
+  }  
 
   public run() {
     this.app.listen(port);
