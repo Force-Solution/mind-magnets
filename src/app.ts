0b7761009805dee4d439579924ff31d5fe272dab
Deepak Yadav <deepaky.tt.19@nitj.ac.in>
2024-02-19 21:38:56 +0530

    code fix

--- src/app.ts
+++ src/app.ts
@@ -41,7 +41,7 @@ export class App {
   private configureMiddleware() {
     AppLogger.configureLogger();
     this.app.use(cors());
-    this.app.options('/*', this.configureOptions); // for preflight check
+    this.app.options('*', this.configureOptions); // for preflight check
     this.app.use(json({ limit: '50mb' }));
     this.app.use(urlencoded({ limit: '50mb', extended: true }));
     this.app.use(logger); // log request
@@ -85,10 +85,10 @@ export class App {
   }
 
   private configureOptions(_request: Request, response: Response) {
-    response.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    response.header('Access-Control-Allow-Origin', '*');
     response.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     response.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-    response.sendStatus(200);
+    response.send(200);
   }  
 
   public run() {
