bcca8bf5f7462543b0ffc4ace835d9bee6a70594
Deepak Yadav <deepaky.tt.19@nitj.ac.in>
2024-02-20 09:46:00 +0530

    fix

--- src/app.ts
+++ src/app.ts
@@ -85,12 +85,12 @@ export class App {
     });
   }
 
-  private configureOptions(_request: Request, response: Response) {
-    response.header('Access-Control-Allow-Origin', '*');
-    response.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    response.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-    response.send(200);
-  }  
+  // private configureOptions(_request: Request, response: Response) {
+  //   response.header('Access-Control-Allow-Origin', '*');
+  //   response.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  //   response.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+  //   response.send(200);
+  // }  
 
   public run() {
     this.app.listen(port);
