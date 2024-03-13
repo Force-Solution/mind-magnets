0bca4da9906a734b569fca8afac1283ca698cc8f
Deepak Yadav <deepaky.tt.19@nitj.ac.in>
2024-03-13 21:35:09 +0530

    schema fixes

--- src/validation/schema/class.ts
+++ src/validation/schema/class.ts
@@ -3,12 +3,12 @@ import Joi from 'joi';
 export default {
   createClass: Joi.object().keys({
     name: Joi.string().required(),
-    description: Joi.string().optional(),
-    backgroundImg: Joi.string().optional(),
-    logoImg: Joi.string().optional(),
+    description: Joi.string().allow('').optional(),
+    backgroundImg: Joi.string().allow('').optional(),
+    logoImg: Joi.string().allow('').optional(),
     startTime: Joi.string().optional(),
     endTime: Joi.string().optional(),
-    students: Joi.array().items(Joi.string().email()).optional(),
+    students: Joi.array().allow([]).items(Joi.string().email()).optional(),
     batch: Joi.string().required()
   }),
 };
